4d0db68ee7906b0031a5f4702a0788e59e34fe03
nutv99 <nutv99@gmail.com>
2022-08-31 09:31:33 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -157,7 +157,7 @@ export class AppComponent implements OnInit, AfterViewInit {
         tap((data) => {
           console.log('Success', data);
         }),
-        delay(200)
+        delay(400)
       )
       .subscribe({
         next: (res) => {
