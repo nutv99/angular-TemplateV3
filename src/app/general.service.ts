7c9c1a7c1d674c0e8564569f529a22374db69fc8
nutv99 <nutv99@gmail.com>
2022-08-31 07:11:15 +0700

    2 files modified

--- src/app/general.service.ts
+++ src/app/general.service.ts
@@ -33,17 +33,16 @@ takeDataPatchByAttribute (dataOnForm:any,dataToPatch:any) {
   const refs = document.querySelectorAll(`[modepatch*="y"]`);
   
   for (let i = 0; i <= refs.length-1; i++) {
-    console.log(refs[i].id);
+    // console.log(refs[i].id);
     elemTemp = document.getElementById(refs[i].id) as HTMLInputElement; 
     stJson = stJson + '"' + refs[i].id + '" : "' +  elemTemp.value + '",' ;
-    console.log('stJson',stJson) ;
+    // console.log('stJson',stJson) ;
 
   }
   
-    st2 = stJson.slice(0, -1) + '}';
-    dataToPatch = JSON.parse(st2) ;
-  
-    return dataToPatch ;
+  st2 = stJson.slice(0, -1) + '}';
+  dataToPatch = JSON.parse(st2) ;  
+  return dataToPatch ;
   
   }
 
