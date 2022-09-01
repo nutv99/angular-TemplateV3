2c9d867cbfc7c271b186df164e04d3c80ea7df43
nutv99 <nutv99@gmail.com>
2022-09-01 08:10:33 +0700

    2 files modified

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,23 +1,24 @@
-"// Table $1  Models "
-"export interface $1_Models {",
-"}",
-"// Model สำหรับ Get ALL",
-"export interface $1_GetALL {",
-  "// รายการ ฟิลด์ จาก Swagger ของ Get ALL",
-"}",
-"// Model สำหรับ Get By ID",
-"export interface $1_ByID {",
-  "// รายการ ฟิลด์ จาก Swagger ของ Get By ID",
-"}",
-"// Model สำหรับ Post",
-"export interface $1_POST {",
- " // รายการ ฟิลด์ จาก Swagger ของ  POST",
-"}",
-"// Model สำหรับ PATCH",
-"export interface $1_POST {",
-  "// รายการ ฟิลด์ จาก Swagger ของ  POST",
-"}",
-"// Model สำหรับ delete",
-"export interface $1_POST {",
-  "// รายการ ฟิลด์ จาก Swagger ของ  POST",
-"}"
+// Table Customer  Models 
+export interface Customer_Models {
+}
+// Model สำหรับ Get ALL
+export interface Customer_GetALL {
+// รายการ ฟิลด์ จาก Swagger ของ Get ALL
+}
+// Model สำหรับ Get Get_By ID
+export interface Customer_ByID {
+// รายการ ฟิลด์ จาก Swagger ของ Get By ID
+}
+// Model สำหรับ Post
+export interface Customer_POST {
+ // รายการ ฟิลด์ จาก Swagger ของ  POST
+}
+// Model สำหรับ PATCH
+export interface Customer_PATCH {
+// รายการ ฟิลด์ จาก Swagger ของ  POST
+}
+// Model สำหรับ delete
+export interface Customer_DELETE {
+// รายการ ฟิลด์ จาก Swagger ของ  POST
+}
+
