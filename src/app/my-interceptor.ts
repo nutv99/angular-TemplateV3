a68965bb9efbec52dda10f8900fec7f70e9cdddb
nutv99 <nutv99@gmail.com>
2022-09-02 07:05:55 +0700

    2 files added

---
+++ src/app/my-interceptor.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpInterceptor, 
+        HttpHandler,
+        HttpRequest} from '@angular/common/http';
+import { Observable } from 'rxjs';      
+
+@Injectable()
+
+export class MyInterceptor implements HttpInterceptor { 
+
+  constructor(){}
+     
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Interceptor ran..');
+    return next.handle(req);
+  }
+}
+
